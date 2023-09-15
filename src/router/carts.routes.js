de388c368988148aaa2adcee96037054f7917f65
BackEnd <dalacristal25@gmail.com>
2023-09-15 19:53:52 -0300

    segunda pre-entrega

--- src/router/carts.routes.js
+++ src/router/carts.routes.js
@@ -110,10 +110,12 @@ cartRouter.delete("/:cid/products/:pid", async (req, res) => {
 cartRouter.put("/:cid", async (req, res) => {
   const { cid } = req.params;
   const arr = req.body;
+
   try {
     const cart = await cartModel.findById(cid);
     arr.forEach((p) => {
-      let i = cart.products.findIndex((prod) => prod.id_prod == p.id_prod);
+      let i = cart.products.findIndex((prod) => prod.id_prod._id == p.id_prod);
+
       if (i != -1) {
         cart.products[i] = p;
       } else {
@@ -130,10 +132,64 @@ cartRouter.put("/:cid", async (req, res) => {
   }
 });
 
-export default cartRouter;
+cartRouter.put("/:cid/products/:pid", async (req, res) => {
+  const { cid, pid } = req.params;
+  const { quantity } = req.body;
 
-/* 
-{
-    "id_prod": "64f3eea179c2827e1bd4da93",
-    "quantity": 25
-} */
+  try {
+    const cart = await cartModel.findById(cid);
+    if (cart) {
+      const prod = await productModel.findById(pid);
+
+      if (prod) {
+        const indice = cart.products.findIndex(
+          (item) => item.id_prod._id == pid
+        );
+        console.log(indice);
+        if (indice != -1) {
+          cart.products[indice].quantity = quantity;
+        } else {
+          cart.products.push({ id_prod: pid, quantity: quantity });
+        }
+        const respuesta = await cartModel.findByIdAndUpdate(cid, cart);
+        res.status(200).send({ respuesta: "OK", mensaje: respuesta });
+      } else {
+        res.status(404).send({
+          respuesta: "Error en actualizar cantidad de producto Carrito",
+          mensaje: "Produt Not Found",
+        });
+      }
+    } else {
+      res.status(404).send({
+        respuesta: "Error en actualizar cantidad de producto Carrito",
+        mensaje: "Cart Not Found",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      respuesta: "Error en actualizar cantidad de producto Carrito",
+      mensaje: error,
+    });
+  }
+});
+
+cartRouter.delete("/:cid", async (req, res) => {
+  const { cid } = req.params;
+
+  try {
+    const cart = await cartModel.findById(cid);
+
+    cart.products = [];
+
+    const respuesta = await cartModel.findByIdAndUpdate(cid, cart);
+    res.status(200).send({ respuesta: "OK", mensaje: respuesta });
+  } catch (error) {
+    res.status(400).send({
+      respuesta: "Error en eliminar todos los productos Carrito",
+      mensaje: error,
+    });
+  }
+});
+
+export default cartRouter;
